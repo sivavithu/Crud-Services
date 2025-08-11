7d7dd33b6667b6241c70d0bd99a3bfa94d1874f9
sivavithu <sivavithu15@live.com>
2025-08-11 19:22:12 +0530

    https

---
+++ CrudServices/Helpers/CustomFontResolver.cs
@@ -0,0 +1,20 @@
+﻿using PdfSharp.Fonts;
+using System.IO;
+
+public class CustomFontResolver : IFontResolver
+{
+    // This method tells PdfSharp the filename for the font family
+    public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+    {
+        // Return the name of the font file you added
+        return new FontResolverInfo("Montserrat-Italic.ttf");
+    }
+
+    // This method reads the font file from the disk
+    public byte[] GetFont(string faceName)
+    {
+        // Correct path to find the file inside your 'fonts' folder
+        string fontPath = Path.Combine(Directory.GetCurrentDirectory(), "fonts", faceName);
+        return File.ReadAllBytes(fontPath);
+    }
+}
